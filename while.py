122142bc44ec858747b6c1468c0999a9dfae27c6
REET <108461026+DE4THSHOT@users.noreply.github.com>
2022-08-20 01:46:37 +0530

    Add files via upload

---
+++ while.py
@@ -0,0 +1,8 @@
+# this simple program describes while loop at basics.
+
+i = 0  # 'i' have been given a value as 0 so i will be by default 0 when the program runs
+while i <= 10:  # this shows while if i is greater or equals to 10
+    # but since 'i' is neither greater nor equals to 0 and thus the condition is false and
+    print(i)  # thus here i with default value is 0 and that gets printed
+    i = i + 1  # but then 1 gets added to I. the programs loops again from the beginning.
+# with the updated value of i that is 1. this keeps on happening till the condition comes out true thus " i <= 10"
